03abc8cca04b1789247e10ada0852aaabd1d0897
mrkresnofatih <90560422+mrkresnofatih@users.noreply.github.com>
2021-11-04 19:22:14 +0700

    dart io, string concatenation

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,22 @@
+import 'dart:io';
+
 void main(List<String> args){
-  print('hello world');
+  int myAge;
+  String greetings = 'hello world';
+  myAge = 23;
+  print(myAge);
+  print(greetings);
+
+  // dynamic
+  dynamic variable = 'hey';
+  print(variable);
+  variable = 34;
+  print(variable);
+
+  //user inputs
+  stdout.write('Your name: ');
+  String name = stdin.readLineSync()!;
+  stdout.write('Your age: ');
+  int age = int.parse(stdin.readLineSync()!);
+  print('Hey I\'m $name, and I\'m $age yo.');
 }
